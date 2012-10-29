b42be5c871be6904dde3b88448da9236c4dcd4ce
turisto <turisto@gmail.com>
2012-10-29 22:11:18 +0200

    nothing to see ...

--- release_prepare.sh
+++ release_prepare.sh
@@ -1,15 +1,15 @@
 #STANDARD VERSION
-git checkout 2.0.x
+git checkout 2.x
 git checkout -b release/2.0.0
 mvn release:prepare -DreleaseVersion=2.0.0 -Dtag=2.0.0 -DdevelopmentVersion=2.0.1-SNAPSHOT --batch-mode -DdryRun=false -Drelease
 mvn release:perform -Drelease
-git checkout 2.0.x
+git checkout 2.x
 git merge --no-ff release/2.0.0
 
 git checkout release/2.0.0
 git reset --hard HEAD~1
 git push --force origin release/2.0.0
-git checkout 2.0.x
+git checkout 2.x
 
 # finally, if & when the code gets deployed to production
 git checkout master
